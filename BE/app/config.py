1afafa023244b7c4634eb73a8c3caf16f77777f2
DikraGuiri <dikra.guiri@gmail.com>
2026-03-07 22:25:14 +0000

    test2

--- BE/app/config.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-load_dotenv()
-
-class Config:
-    MONGO_URI = os.getenv("MONGO_URI")
-    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
-    UPLOAD_FOLDER = os.getenv("UPLOAD_FOLDER", "./uploads")
-    CHROMA_PATH = os.getenv("CHROMA_PATH", "./chroma_db")
